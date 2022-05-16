b00cc7978a3915351ad7fb90cbf8be15fc870520
linghu8812 <linghu8812@163.com>
2022-05-16 09:00:38 +0800

    six model ensamble

--- ensamble.py
+++ ensamble.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
-results_1 = pd.read_csv('result_large_90.63.csv')
-results_2 = pd.read_csv('result_large_flip.csv')
-results_1['prediction'] = (results_1['prediction'].values + results_2['prediction'].values) / 2
+results_1 = pd.read_csv('result_large_90.45.csv')
+results_2 = pd.read_csv('result_large_90.63.csv')
+results_3 = pd.read_csv('result_large_flip_90.30.csv')
+results_4 = pd.read_csv('result_base_89.67.csv')
+results_5 = pd.read_csv('result_no_flip_open_all_90.06.csv')
+results_6 = pd.read_csv('result_open_all_89.93.csv')
+results_1['prediction'] = (results_1['prediction'].values + results_2['prediction'].values +
+                           results_3['prediction'].values + results_4['prediction'].values +
+                           results_5['prediction'].values + results_6['prediction'].values) / 6
 results_1.to_csv('result_ensamble.csv', index=False)
